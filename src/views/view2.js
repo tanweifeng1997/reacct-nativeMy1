58f0d1aa433570f66b0719172659c6c6c6a36989
tanweifeng1997 <2449214887@qq.com>
2020-07-21 15:28:47 +0800

    添加你的注释,一般是一些更改信息

---
+++ src/views/view2.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+export default class view2 extends Component {
+  render() {
+    return (
+      <View>
+        <Text> view2 </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
